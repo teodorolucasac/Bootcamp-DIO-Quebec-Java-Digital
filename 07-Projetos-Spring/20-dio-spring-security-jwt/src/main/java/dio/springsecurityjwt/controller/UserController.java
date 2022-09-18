a45e90a671b5c99afa8906112a535b4b23a1d9a7
Lucas Teodoro <lucasteodoroac@gmail.com>
2022-09-18 12:59:18 -0300

    Exercícios - Adicionando Segurança a uma API REST com Spring Security

---
+++ 07-Projetos-Spring/20-dio-spring-security-jwt/src/main/java/dio/springsecurityjwt/controller/UserController.java
@@ -0,0 +1,20 @@
+package dio.springsecurityjwt.controller;
+
+import dio.springsecurityjwt.model.User;
+import dio.springsecurityjwt.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserService service;
+    @PostMapping
+    public void postUser(@RequestBody User user){
+        service.createUser(user);
+    }
+}
