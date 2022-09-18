acfb379b3c560ec4ce2d3e5ef61575ed895d9f4e
Lucas Teodoro <lucasteodoroac@gmail.com>
2022-09-17 21:03:49 -0300

    Slides e Exercícios - Imersão no Spring Framework com Spring Boot

---
+++ 07-Projetos-Spring/12-spring-ioc-di-beans-autowired/src/main/java/dio/springboot/app/Beans.java
@@ -0,0 +1,11 @@
+package dio.springboot.app;
+
+import com.google.gson.Gson;
+import org.springframework.context.annotation.Bean;
+
+public class Beans {
+//    @Bean
+//    public Gson gson(){
+//        return new Gson();
+//    }
+}
